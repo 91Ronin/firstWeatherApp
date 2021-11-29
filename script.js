77c27cda56ab4c494b331cfa7698adfae5522778
91Ronin <r.barnett091@yahoo.com>
2021-11-29 15:55:40 -0500

    Continued bug fix
    Nearly copied YT tutorial line for line, but still receiving errors/bugs when trying to run "weather.fetchWeather()" in the console.

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 let weather = {
-  // https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=imperial&appid=369494c888c8ee42271c8f4fc1783da4
+  // https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=imperial&appid=369494c888c8ee42271c8f4fc1783da4
 
   apiKey: "369494c888c8ee42271c8f4fc1783da4",
   fetchWeather: function(city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-       city + 
-       "&units=imperial&appid=" + 
-       this.apiKey 
-    )    
-      .then((reponse) => reponse.json())
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=imperial&appid=369494c888c8ee42271c8f4fc1783da4")      
+      .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
-  displayWeather: function(data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    console.log(name,icon,description,temp,humidity,speed);
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector("icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°F";
-    document.querySelector(".humidity").innnerText = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "wind Speed:" + speed + "mph";
+  
+    displayWeather: function(data) {
+      const { name } = data;
+      const { icon,description } = data.weather;
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log(name,icon,description,temp,humidity,speed);
+      
   }
 }
